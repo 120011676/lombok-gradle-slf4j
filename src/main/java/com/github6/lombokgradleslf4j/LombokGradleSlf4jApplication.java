efee0306297627122b6622ec01a504a9ae3aa9d7
Say.li <120011676@qq.com>
2019-09-16 13:30:20 +0800

    init

---
+++ src/main/java/com/github6/lombokgradleslf4j/LombokGradleSlf4jApplication.java
@@ -0,0 +1,13 @@
+package com.github6.lombokgradleslf4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokGradleSlf4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombokGradleSlf4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github6/lombokgradleslf4j/LombokGradleSlf4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github6.lombokgradleslf4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokGradleSlf4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

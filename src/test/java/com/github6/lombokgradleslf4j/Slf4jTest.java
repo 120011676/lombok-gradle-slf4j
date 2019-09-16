cf1d8bee7abfd7aab699e2a00eb218d4c9a1f616
Say.li <120011676@qq.com>
2019-09-16 13:36:02 +0800

    gradle 测试目录下lombok @Slf4j 编译失败，bug例子

---
+++ src/test/java/com/github6/lombokgradleslf4j/Slf4jTest.java
@@ -0,0 +1,13 @@
+package com.github6.lombokgradleslf4j;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class Slf4jTest {
+
+    @Test
+    public void test() {
+        log.trace("test");
+    }
+}
